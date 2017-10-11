7ea665a8e280ae2ba8a448a87f0cc39623821ded
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-10-11 08:31:07 +0000

    Adds functions exercises

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  console.log(name + " is " + ( currentYear - yearOfBirth ) + " years old." )
+}
+
+ageCalculator("Suzie", 1985, 2017);
+ageCalculator("Jack", 2005, 2017);
+ageCalculator("Ali", 2017, 2017);
